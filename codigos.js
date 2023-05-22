dea463c2f094674cd5a1f0b0411e137050225753
durocomopedra <seminariomat23@gmail.com>
2023-05-22 08:52:06 -0300

    uhgegoi

---
+++ codigos.js
@@ -0,0 +1,24 @@
+function ativarNome(){
+    let nome = 'yasmim';
+    console.log(nome)
+    
+
+    document.getElementById('aparecer').innerHTML = nome
+    
+}
+function somarNumeros(){
+    let a = 10;
+    
+    let b = 7;
+
+    let c  = a+b;
+
+
+    
+    document.write(c);
+    console.log(c);
+    document.getElementById('aparecer').innerHTML = c
+   
+    
+}
+
